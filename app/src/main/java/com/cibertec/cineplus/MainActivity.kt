d02ea961f6be4d4c938f250d09ea135aa8273186
Sebastian Bravo <c_sbravo@outlook.com>
2025-10-25 11:58:38 -0500

    Agregando subproceso pelicula, previo a comprar asiento

--- app/src/main/java/com/cibertec/cineplus/MainActivity.kt
+++ app/src/main/java/com/cibertec/cineplus/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.cibertec.cineplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        cambiarFragmento(Inicio())
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.bottom_inicio -> cambiarFragmento(Inicio())
+                R.id.bottom_cines -> cambiarFragmento(Cine())
+                R.id.bottom_dulceria -> cambiarFragmento(Dulceria())
+                R.id.bottom_cuenta -> cambiarFragmento(Cuenta())
+                else -> {
+
+                }
+            }
+            true
         }
+    }
+
+    private fun cambiarFragmento(fragment: Fragment){
+
+        val fm = supportFragmentManager
+        val fragmentTransaction = fm.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayoutInicio, fragment)
+        fragmentTransaction.commit()
     }
 }
